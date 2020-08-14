ac3bf7c9b34f9e7d98635c01981b5438474ffbef
TmTron <martin.trummer@tmtron.com>
2020-08-14 16:19:02 +0200

    added an enum-array: causes an Error: 'A circular dependency has been detected (property key: "roles"). Please, make sure that each side of a bidirectional relationships are using lazy resolvers ("type: () => ClassType").'

--- apps/api/src/app/cats/create-cat.dto.ts
+++ apps/api/src/app/cats/create-cat.dto.ts
@@ -1,7 +1,22 @@
+export enum RoleEnum {
+  Admin = 'Admin',
+  Moderator = 'Moderator',
+  User = 'User'
+}
+
+
 export class CreateCatDto {
   readonly name: string;
 
   readonly age: number;
 
   readonly breed: string;
+
+  /**
+   * using an enum array causes this exception:
+   *
+   *   Error: A circular dependency has been detected (property key: "roles").
+   *   Please, make sure that each side of a bidirectional relationships are using lazy resolvers ("type: () => ClassType").
+   */
+  readonly roles: RoleEnum[];
 }
